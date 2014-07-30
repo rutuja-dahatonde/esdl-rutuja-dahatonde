21d34948d7321a6c367f63a1defa266163c7ef8e
rutuja-dahatonde <rutuja.dahatonde@cumminscollege.in>
2014-07-30 09:10:21 +0530

    Update calci.cpp
    added new add method

--- calci.cpp
+++ calci.cpp
@@ -1,7 +1,10 @@
-# include<iostream>
-using namespace std;
+# include<iostream.h>
 
-int main() {
+void main() {
 cout << "Welcome to GitHub";
-return 0;
+}
+double square(in_number) {
+out_number = in_number + in_number;
+double out_number;
+cout << "Output is :" << out_number;
 }
